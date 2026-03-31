028c7791a8c68cfe13c737a598796e1b65ae8ccb
hanakhalda11-del <hanakhalda11@gmail.com>
2026-03-31 19:32:10 +0700

    Enhance looping.cpp with while loop functionality
    Added while loop for generating random numbers.

--- looping.cpp
+++ looping.cpp
@@ -10,3 +10,16 @@ int main(){
         cout<<"Teknologi Informasi UMY"<<endl;
     }
     
+cout<<endl;
+
+    srand(time(0));
+
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x<<endl;
+        x = rand()%10;
+    }
+
+    
